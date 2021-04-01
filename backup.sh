667c97ea694de5cb3cce02928e56cad0a46386b4
Adrij Shikhar <adrijshikhar85@gmail.com>
2021-04-01 13:27:10 +0530

    Thu Apr  1 01:27:10 PM IST 2021

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ backup_dir="$PWD/dotfiles"
 config_folder="$HOME/.config"
 
 declare -a HOME_FILES=(".i3" ".Xresources" ".xinitrc" "ssh-add.sh" ".zshrc" ".zsh_history.backup" ".zsh_history" ".gitconfig" ".dmenurc" ".doom.d")
-declare -a CONFIG_FILES=("i3status" "dunst" "compton.conf" )
+declare -a CONFIG_FILES=("i3status" "dunst" "compton.conf" "picom.conf" )
 
 
 if [ -d $backup_dir ]
